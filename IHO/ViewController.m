1fa220140891d7d60b6dfbaf533ae2972ba7b8a0
Cynosure <cynosure@Cynosures-MacBook-Pro.local>
2013-11-13 10:00:55 -0700

    Initial Commit

---
+++ IHO/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IHO
+//
+//  Created by Cynosure on 11/13/13.
+//  Copyright (c) 2013 asu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ IHO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IHO
+//
+//  Created by Cynosure on 11/13/13.
+//  Copyright (c) 2013 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
